f218b8b6b053f2f91c14f5a828a3e48dc9a8d871
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-10-02 18:17:20 -0300

    :wrench: chore: create users migration

---
+++ src/migrations/20231002205013-create-users.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  /**
+   * 
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize')} Sequelize 
+   */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type : Sequelize.INTEGER
+      },
+      displayName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull : true
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
